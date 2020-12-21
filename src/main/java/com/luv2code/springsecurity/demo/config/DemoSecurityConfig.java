cdcfcf1af3c0096648492f449922ae9ce55f220e
OuniAbir <ouniabir118@gmail.com>
2020-12-21 02:09:43 +0100

    Add JDBC Database Authentication

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.luv2code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,21 +15,21 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	// add a reference to our security data source
+	@Autowired
+	private DataSource securityDataSource ;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		//add our users for in memory configuration
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-		.withUser(users.username("john").password("test123").roles("EMPLOYE"))
-		.withUser(users.username("mary").password("test123").roles("EMPLOYE" ,"MANAGER"))
-		.withUser(users.username("susan").password("test123").roles("EMPLOYE" , "ADMIN")) ;
+
+		auth.jdbcAuthentication().dataSource(securityDataSource) ;
 		
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		 http.authorizeRequests()
-		 .antMatchers("/").hasRole("EMPLOYE")
+		 .antMatchers("/").hasRole("EMPLOYEE")
 		 .antMatchers("/leaders/**").hasRole("MANAGER")
 		 .antMatchers("/systems/**").hasRole("ADMIN")
 		 .and()
